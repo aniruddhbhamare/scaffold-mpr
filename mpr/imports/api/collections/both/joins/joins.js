7ff94b79ad02b948a7f85f39338c90925f2201d3
sushilshimpi <sushil.shimpi@gmail.com>
2019-01-27 23:48:59 -0700

    feat(init): removed autogen mpr dir

--- mpr/imports/api/collections/both/joins/joins.js
+++
@@ -1,6 +0,0 @@
-import {Invoices} from "/imports/api/collections/both/invoices.js";
-import {Classrooms} from "/imports/api/collections/both/classrooms.js";
-
-// Invoices
-Invoices.join(Classrooms, "classroomId", "classroom", ["name"]);
-
